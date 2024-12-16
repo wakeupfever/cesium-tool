66e649a7d7219b0e7cad966d38c3800e57c21056
xuliang <1874803565@qq.com>
2024-12-16 10:13:59 +0800

    init: cesium 初始化

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+// declare module '*.vue' {
+//   import { DefineComponent } from 'vue'
+//   const component: DefineComponent<{}, {}, any>
+//   export default component
+// }
+
+// declare module 'pinia' {
+//   export interface PiniaCustomProperties {
+//     // add your custom properties here
+//     cesiumView: unknown
+//   }
+// }
